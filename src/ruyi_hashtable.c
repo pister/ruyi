6a716892dcf4cc72ab1ea6549c472ed9823487d0
SongLi Huang <zjuhsl@gmail.com>
2019-08-16 18:19:35 +0800

    add hash table

---
+++ src/ruyi_hashtable.c
@@ -0,0 +1,9 @@
+//
+//  ruyi_hashtable.c
+//  ruyi
+//
+//  Created by Songli Huang on 2019/8/16.
+//  Copyright © 2019 Songli Huang. All rights reserved.
+//
+
+#include "ruyi_hashtable.h"
---
+++ src/ruyi_hashtable.h
@@ -0,0 +1,14 @@
+//
+//  ruyi_hashtable.h
+//  ruyi
+//
+//  Created by Songli Huang on 2019/8/16.
+//  Copyright © 2019 Songli Huang. All rights reserved.
+//
+
+#ifndef ruyi_hashtable_h
+#define ruyi_hashtable_h
+
+#include <stdio.h>
+
+#endif /* ruyi_hashtable_h */
